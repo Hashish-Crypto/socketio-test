5a1ebd3cc3bd59fb6e78f367e96fee05b8548b20
Leandro Acquati <leandroacquati@gmail.com>
2022-03-25 05:59:07 -0300

    broadcast chat message

--- chat/src/server.ts
+++ chat/src/server.ts
@@ -5,25 +5,6 @@ import express from 'express'
 import http from 'http'
 import fs from 'fs'
 
-interface ServerToClientEvents {
-  noArg: () => void
-  basicEmit: (a: number, b: string, c: Buffer) => void
-  withAck: (d: string, callback: (e: number) => void) => void
-}
-
-interface ClientToServerEvents {
-  hello: () => void
-}
-
-interface InterServerEvents {
-  ping: () => void
-}
-
-interface SocketData {
-  name: string
-  age: number
-}
-
 const sourceRoot = __dirname.replace('\\dist', '')
 fs.copyFile(sourceRoot + '\\src\\index.html', __dirname + '\\index.html', (err) => {
   if (err) throw err
@@ -31,7 +12,8 @@ fs.copyFile(sourceRoot + '\\src\\index.html', __dirname + '\\index.html', (err) 
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server)
+// const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(server)
+const io = new Server(server)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
@@ -42,6 +24,11 @@ io.on('connection', (socket) => {
 
   socket.on('disconnect', () => {
     console.log('user disconnected')
+  })
+
+  socket.on('chat message', (msg: string) => {
+    console.log('message: ' + msg)
+    io.emit('chat message', msg)
   })
 })
 
